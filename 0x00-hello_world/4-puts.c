fce4d2e8906a97c89a6a75d5b78d2a9baf60f1b7
oodafeisrael <oodafeisrael@yahoo.com>
2023-02-17 09:16:45 -0800

    task 4 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry
+ * main - print a line of code using puts
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle.\n");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
