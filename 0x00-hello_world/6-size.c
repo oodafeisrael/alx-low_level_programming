c4c358b6949779f6529f7664a202ce475cc36723
oodafeisrael <oodafeisrael@yahoo.com>
2023-02-17 09:53:23 -0800

    task 6 commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,8 +2,6 @@
 /**
  * main - Entry point
  *
-!:mime school
-!:mime school
  * Return: 0 (Success)
  */
 int main(void)
@@ -19,5 +17,6 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-	return (0)i;
+	return (0);
 }
+
