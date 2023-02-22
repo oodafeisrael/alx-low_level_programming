d0a5464cdb0492110973fea37e9ee435cc16154f
oodafeisrael <oodafeisrael@yahoo.com>
2023-02-22 09:08:54 -0800

    task 0 commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Description: To print _putchar to stdout
+ *return: 0 always
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
+
