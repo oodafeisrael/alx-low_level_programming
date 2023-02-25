0ded41ff959adadb6595b4def1e733275a785807
oodafeisrael <oodafeisrael@yahoo.com>
2023-02-25 03:47:52 -0800

    task 3 commit

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+* _putchar - write character cto stdout
+* @c: the character to print
+* Return 1 if true, 0 if false
+* on error, -1 return
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
