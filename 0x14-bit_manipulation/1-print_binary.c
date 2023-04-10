d9d954cc41d80dd86b9b2f97b52acac5a4cc453c
oodafeisrael <oodafeisrael@yahoo.com>
2023-04-10 09:44:15 -0700

    task 1 commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,13 +3,18 @@
 #include <stdio.h>
 /**
 * print_binary - this prints binary equivalent of a given decimal number
-* @n: number to be printed in binary
+* @n: value to be printed in binary
 */
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
+	if (n >> 0)
 	{
-		print_binary(n >> 1);
+		if (n >> 1)
+			print_binary(n >> 1);
 		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
 	}
 }
