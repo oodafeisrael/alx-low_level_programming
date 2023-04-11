e9e1c20c3708cef8be3079a1c4a9d29642d77d35
oodafeisrael <oodafeisrael@yahoo.com>
2023-04-10 20:51:38 -0700

    task 5 commit

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* flip_bits - this returns the number of bits to flip when
+* moving from one  number to another
+* @n: first number
+* @m: second number
+* Return: number of bits
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int num_bits;
+
+	for (num_bits = 0; n || m; n >>= 1, m >>= 1)
+	{
+		if ((n & 1) != (m & 1))
+				num_bits++;
+	}
+	return (num_bits);
+}
