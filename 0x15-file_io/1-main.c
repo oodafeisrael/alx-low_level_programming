c8569417ff130023e62e7cf04441ba52b4afa8b7
oodafeisrael <oodafeisrael@yahoo.com>
2023-04-14 01:23:42 -0700

    task 1 commit

--- 0x15-file_io/1-main.c
+++ 0x15-file_io/1-main.c
@@ -3,7 +3,8 @@
 #include "main.h"
 /**
 * main - check the code
-*
+* @ac: argument count
+* @av: an array of string pointerscontaining command line arguments
 * Return: Always 0.
 */
 int main(int ac, char **av)
