1e8c8848cb17fb19dd2ba29117e450f579360873
oodafeisrael <oodafeisrael@yahoo.com>
2023-05-04 21:24:28 -0700

    task 1 commit

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* print_binary - this function prints binary representation of a number
+* @n: Number to be converted to binary
+*/
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	_putchar((n & 1) + '0');
+}
