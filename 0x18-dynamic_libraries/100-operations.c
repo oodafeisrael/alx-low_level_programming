4533909bbfa7779aec85a5aec4d2c28efe5e555a
oodafeisrael <oodafeisrael@yahoo.com>
2023-08-22 06:59:41 -0700

    task 2 commit

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,27 @@
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+int sub(int a, int b)
+{
+	int difference = (a -b);
+	return (difference);
+}
+
+int div(int a, int b)
+{
+	int quotient = (a/b);
+	return (quotient);
+}
+
+int mod(int a, int b)
+{
+	int rem = (a % b);
+	return (rem);
+}
